658c1ee41e153c45f4bc80ab2655a397e98b64bf
Tissu525 <arturfurtadoc@hotmail.com>
2024-05-26 20:13:51 -0300

    Comentario ClientExeption e Clients

--- PagueMais/Client/ClientException.cs
+++ PagueMais/Client/ClientException.cs
@@ -1,11 +1,14 @@
 namespace Exceptions
 {
+  // Define uma classe para mensagem de ERRO caso CPF igual
   public class ClientCpfAlreadyRegisteredException : Exception
   {
+    // Construtor que cria uma mensagem
     public ClientCpfAlreadyRegisteredException() : base("CPF already registered.")
     {
     }
 
+    // Construtor que aceita uma mensagem e a passa para o construtor base
     public ClientCpfAlreadyRegisteredException(string message) : base(message)
     {
     }
