274dc5bc01e7632b9374b0e2ee2e1d4cfb5a69d7
Tissu525 <arturfurtadoc@hotmail.com>
2024-05-30 18:14:46 -0300

    Regras de Negócio

--- PagueMais/Client/ClientRepository.cs
+++ PagueMais/Client/ClientRepository.cs
@@ -21,6 +21,36 @@ namespace Repositories
       return client;
     }
 
+    public async Task<Client?> FindByIdAsync(Guid id)
+    {
+      return await _context.Clients.FindAsync(id);
+    }
+
+    public async Task RemoveAsync(Client client)
+    {
+      _context.Clients.Remove(client);
+      await _context.SaveChangesAsync();
+    }
+
+    public async Task<Client?> FindByCpfAsync(string cpf)
+    {
+      return await _context.Clients.FirstOrDefaultAsync(client => client.Cpf == cpf);
+    }
+
+    public async Task<Client?> FindByEmailAsync(string email)
+    {
+      return await _context.Clients.FirstOrDefaultAsync(client => client.Email == email);
+    }
+    public async Task<Client?> FindByPhoneAsync(int phone)
+    {
+      return await _context.Clients.FirstOrDefaultAsync(client => client.Phone == phone);
+    }
+    public async Task UpdateAsync(Client client)
+    {
+      _context.Clients.Update(client);
+      await _context.SaveChangesAsync();
+    }
+
     public async Task<Client?> FindByCpf(string Cpf)
     {
       return await _context.Clients.FirstOrDefaultAsync(client => client.Cpf == Cpf);
@@ -34,47 +64,5 @@ namespace Repositories
       return await _context.Clients.FirstOrDefaultAsync(client => client.Phone == Phone);
     }
 
-    public async Task<bool> IsValidCpfAsync(string Cpf)
-    {
-      return await Task.Run(() => IsValidCpf(Cpf));
-    }
-
-    private static bool IsValidCpf(string cpf)
-    {
-
-      cpf = new string(cpf.Where(char.IsDigit).ToArray());
-
-
-      if (cpf.Length != 11)
-        return false;
-
-
-      if (cpf.All(c => c == cpf[0]))
-        return false;
-
-
-      int[] multiplier1 = [10, 9, 8, 7, 6, 5, 4, 3, 2];
-      int sum = 0;
-
-      for (int i = 0; i < 9; i++)
-        sum += int.Parse(cpf[i].ToString()) * multiplier1[i];
-
-      int remainder = sum % 11;
-      int digit1 = remainder < 2 ? 0 : 11 - remainder;
-
-
-      int[] multiplier2 = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2];
-      sum = 0;
-
-      for (int i = 0; i < 10; i++)
-        sum += int.Parse(cpf[i].ToString()) * multiplier2[i];
-
-      remainder = sum % 11;
-      int digit2 = remainder < 2 ? 0 : 11 - remainder;
-
-
-      return cpf.EndsWith($"{digit1}{digit2}");
-
-    }
   }
 }
--- PagueMais/Client/ClientService.cs
+++ PagueMais/Client/ClientService.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using System.Runtime.InteropServices;
 using Entities;
 using Exceptions;
@@ -7,37 +8,169 @@ namespace Services
 {
   public class ClientService(ClientRepository produtoRepository)
   {
-    private readonly ClientRepository _produtoRepository = produtoRepository;
+    private readonly ClientRepository _clientRepository = produtoRepository;
 
     public async Task<IEnumerable<Client>> GetAllAsync()
     {
-      return await _produtoRepository.GetAllAsync();
+      return await _clientRepository.GetAllAsync();
     }
 
+    //Adicionar usuário//
     public async Task<Client> CreateAsync(Client client)
     {
-      var registeredClientCpf = await _produtoRepository.FindByCpf(client.Cpf);
+
+      ValidateClientAge(client.BirthDate);
+      //Teste para ver se o CPF é válido
+
+      if (IsValidCpf(client.Cpf) == false)
+      {
+        throw new ClientInvalidCpfException();
+      }
+
+      //Teste para ver se o CPF se repete
+      var registeredClientCpf = await _clientRepository.FindByCpf(client.Cpf);
 
       if (registeredClientCpf is not null)
       {
         throw new ClientCpfAlreadyRegisteredException();
       }
-      var registeredClientEmail = await _produtoRepository.FindByEmail(client.Email);
+
+      //Teste para ver se o Email se repete
+      var registeredClientEmail = await _clientRepository.FindByEmail(client.Email);
 
       if (registeredClientEmail is not null)
       {
-        throw new ClientCpfAlreadyRegisteredException();
+        throw new ClientEmailAlreadyRegisteredException();
       }
-      var registeredClientPhone = await _produtoRepository.FindByPhone(client.Phone);
+
+      //Teste para ver se o Phone se repete
+      var registeredClientPhone = await _clientRepository.FindByPhone(client.Phone);
 
       if (registeredClientPhone is not null)
       {
-        throw new ClientCpfAlreadyRegisteredException();
+        throw new ClientPhoneAlreadyRegisteredException();
       }
 
+      return await _clientRepository.CreateAsync(client);
+    }
+
+    //Remover Usuário//
+    public async Task RemoveAsync(Guid clientId)
+    {
+      var client = await _clientRepository.FindByIdAsync(clientId);
+
+      if (client == null)
+        throw new ClientNotFoundException();
+
+      await _clientRepository.RemoveAsync(client);
+    }
+
+    //Editar Usuário//
+    public async Task UpdateAsync(Guid clientId, Client updatedClient)
+    {
+      var existingClient = await _clientRepository.FindByIdAsync(clientId);
+
+      //Verificar se o id do cliente existe
+      if (existingClient == null)
+        throw new ClientNotFoundException();
+
+      ValidateClientAge(updatedClient.BirthDate);
+
+      //Verificar se o novo CPF já está em uso por outro cliente
+      var clientWithCpf = await _clientRepository.FindByCpfAsync(updatedClient.Cpf);
+      if (clientWithCpf != null && clientWithCpf.Id != clientId)
+        throw new ClientCpfAlreadyExistsException();
+
+      if (IsValidCpf(updatedClient.Cpf) == false)
+      {
+        throw new ClientInvalidCpfException();
+      }
+
+      //Verificar se o novo email já está em uso por outro cliente
+      var clientWithEmail = await _clientRepository.FindByEmailAsync(updatedClient.Email);
+      if (clientWithEmail != null && clientWithEmail.Id != clientId)
+        throw new ClientEmailAlreadyExistsException();
+
+      //Verificar se o novo telefone já está em uso por outro cliente
+      var clientWithPhone = await _clientRepository.FindByPhoneAsync(updatedClient.Phone);
+      if (clientWithPhone != null && clientWithPhone.Id != clientId)
+        throw new ClientPhoneAlreadyExistsException();
+
+      //Atualize as propriedades do cliente existente com os novos valores
+      existingClient.Name = updatedClient.Name;
+      existingClient.Cpf = updatedClient.Cpf;
+      existingClient.Email = updatedClient.Email;
+      existingClient.Phone = updatedClient.Phone;
+      existingClient.BirthDate = updatedClient.BirthDate;
+
+      await _clientRepository.UpdateAsync(existingClient);
+    }
+
+    public async Task<Client?> GetClientByIdAsync(Guid clientId)
+    {
+      var client = await _clientRepository.FindByIdAsync(clientId);
+
+      if (client == null)
+        throw new ClientNotFoundException();
+
+      return client;
+    }
+
+    //Código para Testar Idade
+    private void ValidateClientAge(DateTime BirthDate)
+    {
+      int age = DateTime.Now.Year - BirthDate.Year;
+      if (BirthDate > DateTime.Now.AddYears(-age)) age--;
+      {
+        if (age < 0)
+        {
+          throw new ClientDateNotAceptedException();
+        }
+        if (age > 120)
+        {
+          throw new ClientAgeExceededException();
+        }
+      }
+    }
+
+    //Código para teste do CPF
+    private static bool IsValidCpf(string cpf)
+    {
+
+      cpf = new string(cpf.Where(char.IsDigit).ToArray());
 
 
-      return await _produtoRepository.CreateAsync(client);
+      if (cpf.Length != 11)
+        return false;
+
+
+      if (cpf.All(c => c == cpf[0]))
+        return false;
+
+
+      int[] multiplier1 = [10, 9, 8, 7, 6, 5, 4, 3, 2];
+      int sum = 0;
+
+      for (int i = 0; i < 9; i++)
+        sum += int.Parse(cpf[i].ToString()) * multiplier1[i];
+
+      int remainder = sum % 11;
+      int digit1 = remainder < 2 ? 0 : 11 - remainder;
+
+
+      int[] multiplier2 = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2];
+      sum = 0;
+
+      for (int i = 0; i < 10; i++)
+        sum += int.Parse(cpf[i].ToString()) * multiplier2[i];
+
+      remainder = sum % 11;
+      int digit2 = remainder < 2 ? 0 : 11 - remainder;
+
+
+      return cpf.EndsWith($"{digit1}{digit2}");
+
     }
   }
+
 }
