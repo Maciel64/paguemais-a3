c893d1f312605f6dea327f973f2a38436c9ad89f
tauan <tauancamara11@gmail.com>
2024-06-03 20:09:35 -0300

    Salvarlixo

--- PagueMaisTest/Cart/CartServiceTest.cs
+++ PagueMaisTest/Cart/CartServiceTest.cs
@@ -17,6 +17,126 @@ namespace Tests
       _productRepository = new Mock<IProductRepository>();
       _purchaseRepository = new Mock<IPurchaseRepository>();
       _cartService = new CartService(_cartRepository.Object, _purchaseRepository.Object, _productRepository.Object);
+   
     }
+    [Fact]
+        public void ShouldGetAllClients()
+        {
+            var mockClients = new List<Client>
+            {
+                new Client("Client 1", "client1@example.com", "12345678901", 123456789),
+                new Client("Client 2", "client2@example.com", "09876543210", 987654321)
+            };
+
+            _clientRepository.Setup(repo => repo.GetAll()).Returns(mockClients);
+
+            var clients = _clientService.GetAll();
+
+            Assert.IsType<List<Client>>(clients);
+            Assert.Equal(mockClients[0].Name, clients.ToList()[0].Name);
+            Assert.Equal(mockClients[1].Name, clients.ToList()[1].Name);
+
+            _clientRepository.Verify(repo => repo.GetAll(), Times.Once);
+        }
+
+        [Fact]
+        public void ShouldGetClientById()
+        {
+            var client = new Client("Client 1", "client1@example.com", "12345678901", 123456789);
+            _clientRepository.Setup(repo => repo.FindById(client.Id)).Returns(client);
+
+            var foundClient = _clientService.GetById(client.Id);
+
+            Assert.Equal(client.Id, foundClient.Id);
+            Assert.Equal(client.Name, foundClient.Name);
+            Assert.Equal(client.Email, foundClient.Email);
+
+            _clientRepository.Verify(repo => repo.FindById(client.Id), Times.Once);
+        }
+
+        [Fact]
+        public void ShouldNotGetClientByUnexistentId()
+        {
+            var clientId = Guid.NewGuid();
+            _clientRepository.Setup(repo => repo.FindById(clientId)).Returns((Client)null);
+
+            Assert.Throws<ClientNotFoundException>(() => _clientService.GetById(clientId));
+
+            _clientRepository.Verify(repo => repo.FindById(clientId), Times.Once);
+        }
+
+        [Fact]
+        public void ShouldCreateClient()
+        {
+            var client = new Client("New Client", "newclient@example.com", "11223344556", 123456789);
+            _clientRepository.Setup(repo => repo.Create(client)).Returns(client);
+
+            var newClient = _clientService.Create(client);
+
+            Assert.Equal(client.Name, newClient.Name);
+            Assert.Equal(client.Email, newClient.Email);
+
+            _clientRepository.Verify(repo => repo.Create(client), Times.Once);
+        }
+
+        [Fact]
+        public void ShouldUpdateClient()
+        {
+            var client = new Client("Client", "client@example.com", "12345678901", 123456789);
+            var updatedClient = new Client("Updated Client", "updated@example.com", "12345678901", 123456789);
+
+            _clientRepository.Setup(repo => repo.FindById(client.Id)).Returns(client);
+            _clientRepository.Setup(repo => repo.Update(client));
+
+            _clientService.Update(client.Id, updatedClient);
+
+            Assert.Equal(client.Name, updatedClient.Name);
+            Assert.Equal(client.Email, updatedClient.Email);
+
+            _clientRepository.Verify(repo => repo.FindById(client.Id), Times.Once);
+            _clientRepository.Verify(repo => repo.Update(client), Times.Once);
+        }
+
+        [Fact]
+        public void ShouldNotUpdateClientWithUnexistentId()
+        {
+            var client = new Client("Client", "client@example.com", "12345678901", 123456789);
+            var updatedClient = new Client("Updated Client", "updated@example.com", "12345678901", 123456789);
+
+            _clientRepository.Setup(repo => repo.FindById(client.Id)).Returns((Client)null);
+
+            Assert.Throws<ClientNotFoundException>(() => _clientService.Update(client.Id, updatedClient));
+
+            _clientRepository.Verify(repo => repo.FindById(client.Id), Times.Once);
+            _clientRepository.Verify(repo => repo.Update(It.IsAny<Client>()), Times.Never);
+        }
+
+        [Fact]
+        public void ShouldDeleteClient()
+        {
+            var client = new Client("Client", "client@example.com", "12345678901", 123456789);
+
+            _clientRepository.Setup(repo => repo.FindById(client.Id)).Returns(client);
+            _clientRepository.Setup(repo => repo.Remove(client));
+
+            _clientService.Remove(client.Id);
+
+            _clientRepository.Verify(repo => repo.FindById(client.Id), Times.Once);
+            _clientRepository.Verify(repo => repo.Remove(client), Times.Once);
+        }
+
+        [Fact]
+        public void ShouldNotDeleteClientWithUnexistentId()
+        {
+            var clientId = Guid.NewGuid();
+
+            _clientRepository.Setup(repo => repo.FindById(clientId)).Returns((Client)null);
+
+            Assert.Throws<ClientNotFoundException>(() => _clientService.Remove(clientId));
+
+            _clientRepository.Verify(repo => repo.FindById(clientId), Times.Once);
+            _clientRepository.Verify(repo => repo.Remove(It.IsAny<Client>()), Times.Never);
+        }
   }
+
 }
--- PagueMaisTest/Purchase/PurchaseServiceTest.cs
+++ PagueMaisTest/Purchase/PurchaseServiceTest.cs
@@ -1,20 +1,76 @@
+using Xunit;
 using Moq;
-using Repositories;
 using Services;
+using Entities;
+using Exceptions;
+using Repositories;
+using System;
+using System.Collections.Generic;
 
 namespace Tests
 {
-  public class PurchaseServiceTest
-  {
-    private readonly Mock<IPurchaseRepository> _purchaseRepository;
-    private readonly Mock<IClientRepository> _clientRepository;
-    private readonly PurchaseService _purchaseService;
-
-    public PurchaseServiceTest()
+    public class PurchaseServiceTests
     {
-      _purchaseRepository = new Mock<IPurchaseRepository>();
-      _clientRepository = new Mock<IClientRepository>();
-      _purchaseService = new PurchaseService(_purchaseRepository.Object, _clientRepository.Object);
+        [Fact]
+        public void GetAll_ShouldReturnAllPurchases()
+        {
+            // Arrange
+            var purchases = new List<Purchase>
+            {
+                new Purchase { Id = Guid.NewGuid(), Total = 100, PaymentMethod = EnumMethods.Credit },
+                new Purchase { Id = Guid.NewGuid(), Total = 200, PaymentMethod = EnumMethods.Debit }
+            };
+
+            var mockPurchaseRepository = new Mock<IPurchaseRepository>();
+            mockPurchaseRepository.Setup(repo => repo.GetAll()).Returns(purchases);
+
+            var purchaseService = new PurchaseService(mockPurchaseRepository.Object, null);
+
+            // Act
+            var result = purchaseService.GetAll();
+
+            // Assert
+            Assert.Equal(2, result.Count);
+        }
+
+        [Fact]
+        public void Create_ValidPurchase_ShouldAddNewPurchase()
+        {
+            // Arrange
+            var purchase = new Purchase { Total = 100, PaymentMethod = EnumMethods.Credit, ClientId = Guid.NewGuid() };
+
+            var mockClientRepository = new Mock<IClientRepository>();
+            mockClientRepository.Setup(repo => repo.FindById(It.IsAny<Guid>())).Returns(new Client());
+
+            var mockPurchaseRepository = new Mock<IPurchaseRepository>();
+            mockPurchaseRepository.Setup(repo => repo.Create(It.IsAny<Purchase>())).Returns(purchase);
+
+            var purchaseService = new PurchaseService(mockPurchaseRepository.Object, mockClientRepository.Object);
+
+            // Act
+            var createdPurchase = purchaseService.Create(purchase);
+
+            // Assert
+            Assert.NotNull(createdPurchase);
+        }
+
+        [Fact]
+        public void Create_InvalidTotal_ShouldThrowException()
+        {
+            // Arrange
+            var purchase = new Purchase { Total = -100, PaymentMethod = EnumMethods.Credit, ClientId = Guid.NewGuid() };
+
+            var mockClientRepository = new Mock<IClientRepository>();
+            mockClientRepository.Setup(repo => repo.FindById(It.IsAny<Guid>())).Returns(new Client());
+
+            var mockPurchaseRepository = new Mock<IPurchaseRepository>();
+
+            var purchaseService = new PurchaseService(mockPurchaseRepository.Object, mockClientRepository.Object);
+
+            // Act & Assert
+            Assert.Throws<PurchaseTotalIsInvalidException>(() => purchaseService.Create(purchase));
+        }
+
+        // Testes para os outros métodos...
     }
-  }
 }
