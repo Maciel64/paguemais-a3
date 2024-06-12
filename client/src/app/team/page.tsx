618d25c228f8ef718f449f16426e4ea047e08f45
Maciel Suassuna <macielsuassuna14@gmail.com>
2024-06-11 21:08:53 -0300

    Adicionada foto de logo

---
+++ client/src/app/team/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+const Team = () => {
+  return (
+    <div className="flex justify-center">
+      <Image src="/file.jpg" alt="Logo" width={200} height={200} />
+    </div>
+  );
+};
+
+export default Team;
