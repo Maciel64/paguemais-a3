531c4e0959b585d1d6a22411bcf73852fc3359a0
Maciel Suassuna <macielsuassuna14@gmail.com>
2024-06-13 09:21:50 -0300

    Frontend finalizado.

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.BACKEND_URL,
+  baseURL: "http://localhost:5151/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
